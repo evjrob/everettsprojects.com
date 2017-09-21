7e89467477e3948e2747028acdda98443cb4b057
Everett Robinson <evjrob@gmail.com>
2017-09-20 19:39:25 -0600

    Added a _drafts folder and R script to automatically render .Rmd files to jekyll posts

---
+++ _drafts/render_post.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+
+# Script borrowed from: http://www.jonzelner.net/jekyll/knitr/r/2014/07/02/autogen-knitr/
+
+input <- commandArgs(trailingOnly = TRUE)
+KnitPost <- function(input, base.url = "/") {
+    require(knitr)
+    opts_knit$set(base.url = base.url)
+    fig.path <- paste0("../figs/", sub(".Rmd$", "", basename(input)), "/")
+    opts_chunk$set(fig.path = fig.path)
+    opts_chunk$set(fig.cap = "center")
+    render_jekyll()
+    print(paste0("../_posts/", sub(".Rmd$", "", basename(input)), ".md"))
+    knit(input, output = paste0("../_posts/", sub(".Rmd$", "", basename(input)), ".md"), envir = parent.frame())
+}
+
+KnitPost(input)
